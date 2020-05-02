3557ba373b709739fd6a9f18c9d0b8cb0e77613a
rishabhishtwal <rishabhishtwal@gmail.com>
2020-05-02 10:04:08 +0530

    Added AccessRights Modules

--- QuickZipWebAPI/Models/DownloadMandate/DownloadMandateGridDetails.cs
+++ QuickZipWebAPI/Models/DownloadMandate/DownloadMandateGridDetails.cs
@@ -7,15 +7,15 @@ namespace QuickZipWebAPI.Models.DownloadMandate
 {
     public class DownloadMandateGridDetails
     {
-        public Boolean IsExcelMandateDownload { get; set; }
-        public Boolean IsSnapMandateDownload { get; set; }
-        public Boolean IsMobileData { get; set; }
+        public Nullable<bool> IsExcelMandateDownload { get; set; }
+        public Nullable<bool> IsSnapMandateDownload { get; set; }
+        public Nullable<bool> IsMobileData { get; set; }
         public string createdon { get; set; }
         public string JPGPath { get; set; }
         public string IsScan { get; set; }
         public string TIPPath { get; set; }
         public string IsPrint { get; set; }
-        public Int64 mandateid { get; set; }
+        public Nullable<Int64> mandateid { get; set; }
         public string BankName { get; set; }
         public string status { get; set; }
         public string Amount { get; set; }
